9942232f8fb64e1b6a05681e24eb0ee8dfb53f4f
DorianCursos <doriancursos@gmail.com>
2024-02-17 00:27:03 +0100

    Rename

---
+++ 24 - Prueba técnica - To Do List/js/scripts.js
@@ -0,0 +1,10 @@
+const allTasks = [
+  {
+    // Número de milisegundos desde el 01/01/1970
+    id: Date.now(),
+    taskName: 'Make a todo app',
+    completed: false
+  }
+];
+
+console.log(allTasks);
